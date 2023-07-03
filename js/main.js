f061bd4efb39bfc41580bbe9d3812a458e1b583f
conrado julio <conradojuliosisnero@gmail.com>
2023-07-03 12:43:38 -0500

    arreglado nuevamente el responsive y el nav en escritorio

--- js/main.js
+++ js/main.js
@@ -1,20 +1,12 @@
-const desplegar = document.querySelector('.abrir__menu');//icono de abrir
-const cerrar = document.querySelector('.quitar__menu');//icono de cerrar
-const menu = document.querySelector('.menu');//menu responsive
+const abrirMenu = document.querySelector('.abrir__menu');
+const cerrarMenu = document.querySelector('.quitar__menu');
+const navegador = document.querySelector('.nav');
 
-// funciones de boton resposive 
-function mostrarMenu(){
-    desplegar.addEventListener('click',()=>{
-        menu.classList.add('active')//muestra el menu
-        cerrar.classList.add('activo');//boton de cerrar menu activo
-        desplegar.classList.add('inactivo');//boton de abrir menu inactivo
+function mostrarMenu() {
+    abrirMenu.addEventListener('click',()=>{
+        navegador.classList.toggle('active');
     })
 }
 
-function ocultarMenu(){
-    cerrar.addEventListener('click',()=>{
-        menu.classList.remove('active')//lo oculta
-        desplegar.classList.remove('inactivo');
-        cerrar.classList.remove('activo');
-    })
-}
+
+mostrarMenu(abrirMenu);
