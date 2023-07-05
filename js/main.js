6f4b2c8baeede1584939ae3504a0dc8b6d9a02d0
conrado julio <conradojuliosisnero@gmail.com>
2023-07-05 06:21:14 -0500

    aagregando mas css al nav responsive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 const abrirMenu = document.querySelector('.abrir__menu');
 const cerrarMenu = document.querySelector('.quitar__menu');
 const navegador = document.querySelector('.nav');
+const menu = document.querySelector('.menu');
 
 function mostrarMenu() {
     abrirMenu.addEventListener('click',()=>{
         navegador.classList.toggle('active');
+        cerrarMenu.classList.toggle('activo');
+        menu.classList.toggle('active');
+    })
+}
+
+function ocultarMenu() {
+    cerrarMenu.addEventListener('click',()=>{
+        navegador.classList.remove('active');
+        abrirMenu.classList.remove();
+        cerrarMenu.classList.toggle('activo');
+        menu.classList.remove('active');
     })
 }
 
